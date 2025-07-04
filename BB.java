31f3efc5c917c1b20d1f09d9eb27cdc88ceff014
Kavin81805 <tysonkavin35@gmail.com>
2025-07-04 18:57:00 +0530

    feat : add new code

---
+++ BB.java
@@ -0,0 +1,24 @@
+import java.util.Scanner;
+
+public class BB {
+
+    public static void main(String[] args) {
+        double a,b,c,d,e;
+        Scanner sacn =new Scanner(System.in);
+
+        System.out.println("Enter the a value :");
+        a=sacn.nextDouble();
+        System.out.println("enter the b value :");
+        b=sacn.nextDouble();
+        System.out.println("Enter the c value :");
+        c=sacn.nextDouble();
+
+        System.out.println("a = "+a+"\nb = "+b+"\nc = "+c);
+        d = (a*b*c);
+        System.out.println("Multiplication of a,b,c is = "+d);
+        c = (a+b+c);
+        System.out.println("Addition of a,b,c = "+c);
+        System.out.println("Now ("+d+") divided by ("+c+") is = "+(d/c));
+    }
+    
+}
